95995a6aae66559f39c26db0f9194572ea01b473
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-12-01 20:14:45 -0300

    feat: adding cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { mongooseClient } from './db/mongoose';
 
 async function startServer() {
   const app = express();
   await mongooseClient();
 
+  app.use(cors);
   app.listen(3333, () => {
     console.log('server is running on http://localhost:3333');
   });
