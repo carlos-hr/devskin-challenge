a780d0821cdf5efbb9d2059bbba407e157011490
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-12-01 20:16:18 -0300

    feat: adding product schema

---
+++ src/models/product.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from 'mongoose';
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+export { Product, productSchema };
