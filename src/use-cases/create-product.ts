265abe0726fd7fa9f66e9b7b0dacb0b1787ebd75
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-12-02 22:00:59 -0300

    fix: adjusting product formating

--- src/use-cases/create-product.ts
+++ src/use-cases/create-product.ts
@@ -1,12 +1,14 @@
 import { IProductRequest } from '@/@types/IProduct';
 import { ProductRepository } from '@/repositories/product';
+import { productFormatter } from '@/utils/productFormatter';
 
 export class CreateProductUseCase {
   constructor(private productRepository: ProductRepository) {}
 
   async execute(data: IProductRequest) {
     const product = await this.productRepository.create(data);
+    const formatedProduct = productFormatter(product);
 
-    return { product };
+    return { product: formatedProduct };
   }
 }
